58128ca99f5a38301048de28f4c6502b70e5d820
pmartinet44 <pmartinet44@546fa5e4-0eb1-2148-f228-0106d79736e1>
2012-10-20 09:32:03 +0000

    Ecriture de bombe.js / Mise à jour des tâches Mise en place pas encore effectué dans le main Disfonctionnement normal ;)

--- src/BO/Bombe.js
+++ src/BO/Bombe.js
@@ -1,5 +1,9 @@
 //-----------------------------------------------------------------------------
-// Bombe.js
+// Bombe.js			:  Classe basique pour une bombe
+//
+//	Author : Pizzi 
+//	Dcreat : 20/10/2012
+//	Dmodif : 20/10/2012 11:25
 //-----------------------------------------------------------------------------
 
 /// <summary>
@@ -8,12 +12,18 @@
 (function (window) {
 
     //Attributs de la classe Bombe
-    this.TEMPS_EXPLOSION;
-    this.temps;
-    this.index;
+    this.temps_explosion;  // Temps avant que la bombe explose
+	this.temps_rouge;  // Temps avant que la bombe rougit
+    this.temps;	// Temps écoulé depuis la pause de la bombe
+    this.index;   // Index de la bombe dans le tableau
+	this.rectangle;  // Rectangle de la bombe collision
+	this.imgBombe;  // Image de la bombe à l'initialisation
+	this.imgBombeRouge; // Image de la bombe juste avant l'explosion
+	this.width  // Largeur de la bombe
+	this.height // Hauteur de la bombe
 
-    function Bombe(imgBombe, position) {
-        this.initialize(imgBombe, position);
+    function Bombe(imgBombe, imgBombeRouge, position, temps_explosion) {
+        this.initialize(imgBombe, imgBombeRouge, position, temps_explosion);
     }
 
     // Using EaselJS BitmapSequence as the based prototype
@@ -23,32 +33,56 @@
     //unique to avoid overiding base class
     Bombe.prototype.Bitmap_initialize = Bombe.prototype.initialize;
 
-    Bombe.prototype.initialize = function (imgBombe, position) {
+	// Initialisation de la bombe
+    Bombe.prototype.initialize = function (imgBombe, imgBombeRouge, position, temps_explosion) {
         this.Bitmap_initialize(imgBombe);
+		// --- Position et taille de la bombe
         this.x = position.x;
         this.y = position.y;
+		this.width = imgBombe.width;
+		this.height = imgBombe.height;
+		// --- Les deux images de la bombe
+		this.imgBombe = imgBombe;
+		this.imgBombeRouge = imgBombeRouge;
+		// -- Les différentes Timers de la bombe
         this.temps = 0;
-		this.TEMPS_EXPLOSION = 100;
-        //this.index = nbr_bombes;
+		this.temps_explosion = temps_explosion;
+		this.temps_explosion = temps_explosion - (temps_explosion/10); 
+		// -- Initialisation de l'index de la bombe
 		this.index = tab_bombes.getNbBombes();
-		//nbr_bombes++;
-        //this.AjouterBombe();
+		// -- Initialisation du rectange de la bombe
+		this.rectange = new XNARectangle(this.x, this.y, this.width, this.height);
+		// On fini en ajoutant la bombe sur le dessin
 		tab_bombes.add(this);
     }
 
-
+	// Boucle sur une bombe
     Bombe.prototype.tick = function () {
         this.temps += 1;
-        if (this.temps == this.TEMPS_EXPLOSION) {
-            //SupprimerBombe(this.index);
+		// -- On vérifie si la bombe doit rougir
+		if (this.temps >= this.temps_explosion - (this.temps_explosion/10)
+			this.RougirBombe();
+		// -- On vérifie si la bombe doit exploser
+        if (this.temps >= this.TEMPS_EXPLOSION) {
             tab_bombes.deleteAtIndex(this.index);
+			// On enlève la bombe du dessin
             stage.removeChild(this);
         }
     }
-
-    //Bombe.prototype.AjouterBombe = function () {
-    //    stage.addChild(this);
-    //}
-
+	
+	// Faire rougir la bombe (signal de l'explosion imminente de la bombe
+	Bombe.prototype.RougirBombe = function () {
+		this.Bitmap_initialize(imgBombeRouge);
+		this.width = imgBombeRouge.width;
+		this.height = imgBombeRouge.height;
+		this.rectange = new XNARectangle(this.x, this.y, this.width, this.height);
+	}
+	
+	// Actualise le rectangle de la bombe
+	Bombe.prototype.GetRectangle = function () {
+		this.rectangle = new XNARectangle(this.x, this.y, this.width, this.height);
+		return this.rectangle;
+	}
+    
     window.Bombe = Bombe;
 }(window));
